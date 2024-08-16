37497538238291a22c7bbcf553b084d52812f9ba
Nathan Folsom <n8folsom@gmail.com>
2024-08-15 18:23:39 -0700

    fix comment/whitespace parsing

--- interpreter/src/tokens/token.rs
+++ interpreter/src/tokens/token.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub enum Token {
     Keyword(String),
     U32(u32),
--- interpreter/src/tokens/tokenizer.rs
+++ interpreter/src/tokens/tokenizer.rs
@@ -1,21 +1,58 @@
-use super::{token::Token, whitespace::WhitespaceParser};
+use super::{
+    token::Token,
+    whitespace::{
+        close_block_comment, is_whitespace, open_block_comment, open_line_comment, CARRIAGE_RETURN,
+        LINE_FEED,
+    },
+};
 
 pub fn from_text(input: String) -> Vec<Token> {
     let mut output = vec![];
-    let mut whitespace_parser = WhitespaceParser::new();
-    let mut prev = None;
-    input.chars().for_each(|c| {
-        if whitespace_parser.is_whitespace(c, prev) {
-            return;
+    let chars: Vec<char> = input.chars().collect();
+    let mut i = 0;
+    while i < chars.len() {
+        let current = chars.get(i).unwrap();
+        let next = chars.get(i + 1);
+        if is_whitespace(current) {
+            continue;
         }
-
-        if c == '(' {
+        if open_block_comment(current, next) {
+            i += 2;
+            let mut depth = 0;
+            while i < chars.len() {
+                let current = chars.get(i).unwrap();
+                let next = chars.get(i + 1);
+                if open_block_comment(current, next) {
+                    depth += 1;
+                } else if close_block_comment(current, next) {
+                    if depth == 0 {
+                        break;
+                    } else {
+                        depth -= 1;
+                    }
+                }
+                i += 1;
+            }
+            i += 2;
+            continue;
+        } else if open_line_comment(current, next) {
+            i += 2;
+            while i < chars.len() {
+                let current = chars.get(i).unwrap();
+                if current == &CARRIAGE_RETURN || current == &LINE_FEED {
+                    break;
+                }
+                i += 1;
+            }
+            i += 1;
+            continue;
+        } else if current == &'(' {
             output.push(Token::OpenParenthesis);
-        } else if c == ')' {
+        } else if current == &')' {
             output.push(Token::CloseParenthesis);
         }
-        prev = Some(c);
-    });
+        i += 1;
+    }
     output
 }
 
@@ -25,8 +62,18 @@ pub fn from_binary(input: Vec<u8>) -> Vec<Token> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn should_parse_keyword() {
-        todo!();
+    fn should_read_line_comment() {
+        let output = from_text(";; line comment \n".to_string());
+        assert_eq!(output.len(), 0);
+    }
+
+    #[test]
+    fn should_read_block_comment() {
+        let output = from_text("(; block comment ;)".to_string());
+        println!("{:?}", output);
+        assert_eq!(output.len(), 0);
     }
 }
